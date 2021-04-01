b5909bcbe4b9e339680dabf7b12bde64c216fbce
raphaelw <raphaelw@users.noreply.github.com>
2021-04-01 18:08:47 +0200

    cleanup

--- 03_week/project/loss_function_3d_plot.py
+++ 03_week/project/loss_function_3d_plot.py
@@ -15,30 +15,34 @@ def model(w0, w1, x):
 
 def lasso_error(w0, w1, x_true, y_true, alpha=1.0):
     y_pred = model(w0, w1, x)
-    error = mse(y_true,y_pred) #+ alpha*( abs(w0)+abs(w1) )
+    error = mse(y_true,y_pred) + alpha*( abs(w0)+abs(w1) )
     return error
 
 #-------------------------------------------------
 
-x     = np.random.randn(150)
-noise = np.random.randn(150) * 0.4
+# generate noisy data on a line:
+w0    = 25.0 # intercept
+w1    = 0.0 # slope
+num_samples = 150
+x     = np.random.randn(num_samples)
+noise = np.random.randn(num_samples) * 0.4
+y = w1*x + w0 + noise
 
-y = 25.0*x + noise
-
+# calculate error for many pairs of w0 and w1
 limit = 50
-snum = 100
-all_w0 = np.linspace(start=-limit, stop=limit, num=snum)
-all_w1 = np.linspace(start=-limit, stop=limit, num=snum)
-all_w0, all_w1 = np.meshgrid(all_w0, all_w1)
-all_errors = np.zeros_like(all_w0) # Z
+num_samples = 100
+X = np.linspace(start=-limit, stop=limit, num=num_samples)
+Y = np.linspace(start=-limit, stop=limit, num=num_samples)
+X, Y = np.meshgrid(X, Y)
+Z = np.zeros_like(X)
 
-for i1 in range(all_errors.shape[0]):
-    for i2 in range(all_errors.shape[1]):
-        w0 = all_w0[i1,i2]
-        w1 = all_w1[i1,i2]
+for i1 in range(Z.shape[0]):
+    for i2 in range(Z.shape[1]):
+        w0 = X[i1,i2]
+        w1 = Y[i1,i2]
 
         error = lasso_error(w0=w0, w1=w1, x_true=x, y_true=y, alpha=1.0)
-        all_errors[i1,i2] = error
+        Z[i1,i2] = error
 
 
 #fig, (ax1, ax2) = plt.subplots(ncols=2)
@@ -50,9 +54,9 @@ fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 # create light source object.
 ls = LightSource()
-rgb = ls.shade(all_errors, plt.cm.RdYlBu)
+rgb = ls.shade(Z, plt.cm.RdYlBu)
 
-surf = ax.plot_surface(all_w0, all_w1, all_errors, rcount=500, ccount=500, linewidth=0, facecolors=rgb, antialiased=False, shade=True, lightsource=ls) #viridis
+surf = ax.plot_surface(X, Y, Z, rcount=500, ccount=500, linewidth=0, facecolors=rgb, antialiased=False, shade=True, lightsource=ls) #viridis
 #ax.scatter(0,1,lasso_error(10,0,x,y,alpha=1.0), c='green')
 ax.set_xlabel('intercept w0')
 ax.set_ylabel('slope w1')
