9b0beeb5770d9acd536fd903fb5b2be82d262ea7
raphaelw <raphaelw@users.noreply.github.com>
2021-04-07 21:51:08 +0200

    catch more duplicates

--- 04_week/project/lyrics_scraping/lyrics_scraping/pipelines.py
+++ 04_week/project/lyrics_scraping/lyrics_scraping/pipelines.py
@@ -9,6 +9,11 @@ from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 
 from hashlib import md5
+import re
+
+def normalize_song_title(title):
+    "In the Waiting Line [Live] -> In the Waiting Line"
+    return re.sub(r"\[[^\]]*\]", '', title).strip()
 
 class DuplicateSongsPipeline:
     def __init__(self):
@@ -17,7 +22,8 @@ class DuplicateSongsPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
-        text = adapter['artist_found'].lower().strip() + adapter['song_title'].lower().strip()
+        song_title_normalized = normalize_song_title(adapter['song_title'])
+        text = adapter['artist_found'].lower().strip() + song_title_normalized.lower().strip()
         hash_sum = md5(text.encode("utf-16")).hexdigest()
 
         if hash_sum in self.hashes_seen:
