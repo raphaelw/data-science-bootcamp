e85c847330025eb6785a6aad16e9b811ed7772f7
raphaelw <raphaelw@users.noreply.github.com>
2021-05-05 17:01:26 +0200

    rotating circle

--- 08_week/project/simulation.py
+++ 08_week/project/simulation.py
@@ -1,9 +1,16 @@
+import math, cmath
 import numpy as np
 import cv2
 
+"""
+use parabola y = -(x*2-1)^2 + 1
+draw stuff: https://docs.opencv.org/master/dc/da5/tutorial_py_drawing_functions.html
+choice: https://stackoverflow.com/questions/40927221/how-to-choose-keys-from-a-python-dictionary-based-on-weighted-probability/40927437
+"""
 
 TILE_SIZE = 32
 OFS = 50
+FPS = 25
 
 MARKET = """
 ##################
@@ -20,6 +27,44 @@ MARKET = """
 ##############GG##
 """.strip()
 
+class Ramp:
+    """Ramps a float from 0 to 1 within n ticks."""
+    def __init__(self):
+        self.value = 0.
+        self.step_size = 0.
+
+    def reset(self, n_ticks):
+        self.value = 0.
+        self.step_size = 1./n_ticks
+
+    def tick(self):
+        self.value += self.step_size
+        self.value = min(1.,self.value)
+        return self.value
+    
+    def value(self):
+        return self.value
+
+    def done(self):
+        if float(self.value) == float(1):
+            return True
+        return False
+
+class Customer:
+    def __init__(self):
+        self.pos = 1.
+        self._m = cmath.exp(1.j * 0.1)
+
+    def tick(self):
+        pass
+    
+    def draw(self, frame):
+        self.pos *= self._m
+        self.pos /= abs(self.pos) # stabilize
+
+        cpos = self.pos * 60. + (100+100j)
+        cpos = (round(cpos.real), round(cpos.imag))
+        cv2.circle(frame, cpos, 4, (0,0,255), -1)
 
 class SupermarketMap:
     """Visualizes the supermarket background"""
@@ -84,19 +129,22 @@ class SupermarketMap:
 
 
 if __name__ == "__main__":
+    frame_interval_ms = int(1000./FPS)
 
     background = np.zeros((700, 1000, 3), np.uint8)
     tiles = cv2.imread("tiles.png")
 
+    customer = Customer()
     market = SupermarketMap(MARKET, tiles)
 
     while True:
         frame = background.copy()
-        market.draw(frame)
+        #market.draw(frame)
+        customer.draw(frame)
 
         cv2.imshow("frame", frame)
 
-        key = chr(cv2.waitKey(1) & 0xFF)
+        key = chr(cv2.waitKey(delay=frame_interval_ms) & 0xFF)
         if key == "q":
             break
 
