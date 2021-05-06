856266104be667c9f06c5e30fc0ba2a762e8cfb6
raphaelw <raphaelw@users.noreply.github.com>
2021-05-07 00:36:25 +0200

    fully working simulation

--- 08_week/project/customer.py
+++ 08_week/project/customer.py
@@ -1,6 +1,54 @@
 import random
+import json
+
+import numpy as np
 
 class CustomerModel:
+    """Cutomer dummy model. Completely random transitions."""
+    def __init__(self, customer_id=1):
+        with open('transition_matrix.json', 'rb') as f:
+            self._transition_matrix = json.load(f)
+        with open('mean_durations.json', 'rb') as f:
+            self._mean_durations = json.load(f)
+
+        self.state = 'entrance'
+        self.duration = 1.
+        self.duration = customer_id*1.
+
+    def get_state(self):
+        """Returns tuple containing: state, duration"""
+        return self.state, self.duration
+
+    def next_state(self):
+        """ Transition to next state.
+        Returns tuple containing: state, duration"""
+        if self.state == 'checkout':
+            self.state = None
+            self.duration = 0.
+        
+        if self.state is None:
+            return
+        
+        # Monte Carlo
+        last_state = self.state
+        probability_mass_function = self._transition_matrix[last_state]
+        self.state = random.choices(population=list(probability_mass_function.keys())
+                                    , weights=probability_mass_function.values()
+                                    , k=1)[0]
+        
+        # exponential distribution sampling for time spent in a section
+        if self.state == 'checkout':
+            self.duration = 1.5
+        else:
+            mean = self._mean_durations[self.state]
+            lambd = 1/mean
+            self.duration = random.expovariate(lambd=lambd)
+
+    def done(self):
+        return self.state is None
+
+
+class CustomerModelRandom:
     """Cutomer dummy model. Completely random transitions."""
     def __init__(self, customer_id=1):
         self.state = 'entrance'
@@ -31,3 +79,10 @@ class CustomerModel:
     
     def done(self):
         return self.state is None
+
+
+if __name__ == '__main__':
+    c = CustomerModel(22)
+    print(c._transition_matrix['checkout'])
+    print('')
+    print(c._durations)
